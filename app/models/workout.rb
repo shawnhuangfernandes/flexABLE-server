5b1ebed4cf860382bfcca4d7f31fffaac490287e
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-11 19:45:30 -0800

    Updated Workout Model to search for workouts based on a user and date

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -7,22 +7,25 @@ class Workout < ApplicationRecord
     validates :user_id, presence: true # validation to make sure user is specified
     validates :exercise_id, presence: true # validation to make sure exercise is specified
 
-    def self.workouts_of_the_week(:user_id, :day_of_the_week, :day, :month, :year)
+    def self.workouts_of_the_week(user_id, day, month, year)
         workoutsArray=Array.new(7) # first create a workout array of size 7 to hold the workouts of the week
-        current_day = day - day_of_the_week # then lets start from the beginning of the week (Monday is the start)
-        workoutsArray.map.with_index do |val, index| # go through each day of the week
-            find_by_user_and_date(user_id: user_id, current_day: current_day, month: month, year: year) # return the workout created by the user on that day
-            current_day += 1;
-        end
+        today = Date.new(year, month, day) # get today's date
+        $current_date = today - today.wday # then lets start from the beginning of the week (Monday is the start)
 
+        workoutsArray.map.with_index do |val, index| # go through each day of the week
+            workout_search_result = find_by_user_and_date(user_id, $current_date) # return the workout created by the user on that day
+            $current_date = Date.new($current_date.year, $current_date.month, $current_date.day) + 1 # increment the current day
+            workout_search_result
+        end
     end
 
-    def self.find_by_user_and_date(:user_id, :day, :month, :year)
-        # find all the workouts created by the user
-        # look through all the workouts of that user
-            # if any of those workouts contain a DATE object that is of the day, month, and year
-                # return it
-        # return null
-        workouts_of_user = Workout.find_by(user_id: user_id)
+    def self.find_by_user_and_date(user_id, current_date)
+        workouts_of_user = Workout.where(user_id: user_id)  # find all the workouts created by the user
+
+        # detect first occurrence of workout of user
+        workouts_of_user.select do |workout|
+            w_date = workout.workout_date
+            w_date.day == $current_date.day && w_date.month == $current_date.month && w_date.year == $current_date.year
+        end
     end
 end
