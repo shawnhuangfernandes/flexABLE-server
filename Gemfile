b24cd479bf61fdabc58b8c93d49dcb72fffeef4a
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-27 10:39:08 -0800

    starting new app

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'jwt'
 #Figaro gem to hide secret key
 gem 'figaro'
 
+#Gem for hosting
+gem 'foreman'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ public/precache-manifest.fc480fc03e71b4218f1f4f9a9c742562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0e4ee69b0d039caa854b0ed4221fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad8c0108e4579c7dc0c4",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "d8e7c69e449259fbb6c3",
+    "url": "/static/css/main.0b8e9a84.chunk.css"
+  },
+  {
+    "revision": "ad8c0108e4579c7dc0c4",
+    "url": "/static/js/2.0da63298.chunk.js"
+  },
+  {
+    "revision": "078b3176bf356d98c3a4140bc57ef542",
+    "url": "/static/js/2.0da63298.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8e7c69e449259fbb6c3",
+    "url": "/static/js/main.f65c44ce.chunk.js"
+  },
+  {
+    "revision": "9c207c5d234d57dde18a",
+    "url": "/static/js/runtime-main.f0fb9469.js"
+  }
+]);
